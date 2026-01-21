deef31ff3553ae9446d78e6a0ab4bec3e69c55eb
cocojisoo <yjs1019a@gmail.com>
2026-01-21 13:27:24 +0900

    initial commit

---
+++ src/main/java/com/example/DuZZonCoo/DuZZonCooApplication.java
@@ -0,0 +1,13 @@
+package com.example.DuZZonCoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuZZonCooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuZZonCooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DuZZonCoo/DuZZonCooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DuZZonCoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuZZonCooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
