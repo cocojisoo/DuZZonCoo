82b3c62715c3487fcde8f600dbc800136b7a5b1f
양지수 <yjs1019a@gmail.com>
2026-01-22 18:08:40 +0900

    feat(domain) : BoardRepository 추가

---
+++ src/main/domain/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.example.DuZZonCoo.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface BoardRepository extends JpaRepository<Board, UUID>{
+    List<Board> findAllByOrderByCreatedAtDesc();
+    List<Board> findAllByTitleContaining(String title);
+    List<Board> findByWriter(String writer);
+}
